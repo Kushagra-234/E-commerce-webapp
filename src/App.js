fdc41a1cdbf50f9185ab2547ffb979b64faaff38
kushagra gupta <kushagraguptastud.02@gmail.com>
2023-03-22 02:14:38 +0530

    initialcommit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Home from './Home'
+
+const App = () => {
+  return (
+   
+   <BrowserRouter>
+   <Routes>
+
+     <Route path="/"  element={<Home/>}/>
+   </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
